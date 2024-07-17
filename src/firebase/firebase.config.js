06cdff5e00e2d265d83e8bc11c00490f53b109d5
tanjiina <tanjina1042@gmail.com>
2024-07-17 13:52:21 +0600

    after change

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBfB5N0v7n3AUplmPyR1w9IQDN9CRqTQi8",
-  authDomain: "expresso-client.firebaseapp.com",
-  projectId: "expresso-client",
-  storageBucket: "expresso-client.appspot.com",
-  messagingSenderId: "469875053380",
-  appId: "1:469875053380:web:c84c2297e21bbe68c17d11"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
